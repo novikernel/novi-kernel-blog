a8260b69f5d8b3b8a1f5e669feeffd0de57f23c1
Novi Kernel <novikernel@gmail.com>
2025-12-08 19:14:40 +0530

    Updates

--- ga.js
+++ ga.js
@@ -1,4 +1,15 @@
+// Initialize dataLayer
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag(){ window.dataLayer.push(arguments); }
+
+// --- Consent Mode default (deny until user accepts) ---
+gtag('consent', 'default', {
+  ad_storage: 'denied',
+  ad_user_data: 'denied',
+  ad_personalization: 'denied',
+  analytics_storage: 'denied'
+});
+
+// Standard GA setup
 gtag('js', new Date());
-gtag('config', 'G-3SB290CFLX');
+gtag('config', 'G-3SB290CFLX', { anonymize_ip: true });
